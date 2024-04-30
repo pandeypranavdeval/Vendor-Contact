419432611094e9725b7f5fea138567aefce7a7c8
pranav deval pandey <pranavdevalp@gmail.com>
2024-04-30 20:22:47 +0530

    first commit

---
+++ js/main.js
@@ -0,0 +1,100 @@
+function sendEmail() {
+	var subject = "Subject of the email";
+	var body = "Content of the email";
+
+	// Encode subject and body for URL
+	var encodedSubject = encodeURIComponent(subject);
+	var encodedBody = encodeURIComponent(body);
+
+	// Construct the mailto link
+	var mailtoLink = "mailto:pranavdevalp2gmail.com?subject=" + encodedSubject + "&body=" + encodedBody;
+
+	// Open the default email client with pre-filled subject and body
+	window.location.href = mailtoLink;
+} 
+
+(function($) {
+
+	"use strict";
+
+
+  // Form
+	var contactForm = function() {
+		if ($('#contactForm').length > 0 ) {
+			$( "#contactForm" ).validate( {
+				rules: {
+					name: "required",
+					subject: "required",
+					email: {
+						required: true,
+						email: true
+					},
+					message: {
+						required: true,
+						minlength: 5
+					}
+				},
+				messages: {
+					name: "Please enter your name",
+					subject: "Please enter your subject",
+					email: "Please enter a valid email address",
+					message: "Please enter a message"
+				},
+				/* submit via ajax */
+				
+				submitHandler: function(form) {		
+					var $submit = $('.submitting'),
+						waitText = 'Submitting...';
+
+					$.ajax({   	
+				      type: "POST",
+				      url: "php/sendEmail.php",
+				      data: $(form).serialize(),
+
+				      beforeSend: function() { 
+				      	$submit.css('display', 'block').text(waitText);
+				      },
+				      success: function(msg) {
+		               if (msg == 'OK') {
+		               	$('#form-message-warning').hide();
+				            setTimeout(function(){
+		               		$('#contactForm').fadeIn();
+		               	}, 1000);
+				            setTimeout(function(){
+				               $('#form-message-success').fadeIn();   
+		               	}, 1400);
+
+		               	setTimeout(function(){
+				               $('#form-message-success').fadeOut();   
+		               	}, 8000);
+
+		               	setTimeout(function(){
+				               $submit.css('display', 'none').text(waitText);  
+		               	}, 1400);
+
+		               	setTimeout(function(){
+		               		$( '#contactForm' ).each(function(){
+											    this.reset();
+											});
+		               	}, 1400);
+			               
+			            } else {
+			               $('#form-message-warning').html(msg);
+				            $('#form-message-warning').fadeIn();
+				            $submit.css('display', 'none');
+			            }
+				      },
+				      error: function() {
+				      	$('#form-message-warning').html("Something went wrong. Please try again.");
+				         $('#form-message-warning').fadeIn();
+				         $submit.css('display', 'none');
+				      }
+			      });    		
+		  		} // end submitHandler
+
+			});
+		}
+	};
+	contactForm();
+
+})(jQuery);
